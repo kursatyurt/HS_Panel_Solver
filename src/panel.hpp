0c464b80a64414610a8be69ccd85848f16e00ed9
kursatyurt <kursatyurt@gmail.com>
2021-02-07 17:14:16 +0100

    Add Panels to Code

---
+++ src/panel.hpp
@@ -0,0 +1,22 @@
+#ifndef PANEL_H
+#define PANEL_H
+
+#include <cmath>
+#include <vector>
+#include "point.hpp"
+
+class Panel
+{
+public:
+    uint panel_num;
+    double xm;
+    double ym;
+    double sinthe;
+    double costhe;
+    Panel() {};
+    Panel(int i, const std::vector<Point> &point);
+    void update(const std::vector<Point> &point);
+};
+
+
+#endif 
