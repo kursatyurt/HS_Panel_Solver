5a2d958a09f0d46ad4c5fd16a1fc0bfaa4f0c868
kursatyurt <kursatyurt@gmail.com>
2021-02-08 04:51:35 +0100

    Correct corrupted file

--- src/panel.hpp
+++ src/panel.hpp
@@ -18,28 +18,4 @@ public:
     void update(const std::vector<Point> &point);
 };
 
-#endi{
- 
-std::cout << "Welcome to Great HS Solver \n";
-std::unique_ptr<BaseInitializer> pInit = std::make_unique<InitFromFile>();
-std::vector<Point> Points;
-std::vector<Panel> Panels; 
-
-pInit->init_points(Points);
-pInit->init_panels(Points,Panels);
- 
-double AoA; 
-std::cout << "Please Enter AoA in Degree:\n" ;
-std:: cin >> AoA; 
-// Convert Angle to Radians
-AoA = AoA * acos(0.0)/90 ;
-
-
-std::unique_ptr<Solver> pSolver = std::make_unique<Solver>();
-pSolver->fill(Points,Panels,AoA);
-pSolver->solve();
-
-
-return 0;
-}
-f
+#endif
