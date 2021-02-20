5340b36d9d1b3c7dee04199a3d4f42bd0511a4d8
kursatyurt <kursatyurt@gmail.com>
2021-02-20 01:32:44 +0100

    Add Unsymmetric NACA4 Digit Airfoils
    Correct thickness for symmetric NACA4 Digit Airfoils

--- src/InitFromNaca.cpp
+++ src/InitFromNaca.cpp
@@ -1,49 +1,143 @@
 #include "InitFromNaca.hpp"
 
-void InitFromNaca::initPoints(std::vector<Point> &Points){
+
+
+void InitFromNaca::initPoints(std::vector<Point> &Points)
+{
   int NACA, num_panel;
   std::cout << "NACA Airfoil (4-5 Digit), Number of Panels: \n";
-  std::cin >> NACA >> num_panel; 
-  
-  if (NACA > 99999 || NACA < 1);
-  {std::runtime_error("Invalid Airfoil Selection!.");}
-  
-  // Create a cosine-distribution
+  std::cin >> NACA >> num_panel;
 
-  auto dist = arma::linspace(0,2.*acos(0.0),num_panel/2+1);
-  dist = 0.5* (1. - cos(dist));  
-  
-  // Decompose it 
-  int a = NACA/10000;
-  int b = (NACA/1000)%10;
-  int c = (NACA/100)%10;
-  int d = (NACA/10)%10;
-  int e = NACA%10;
+  if (NACA > 99999 || NACA < 1)
+    ;
+  {
+    std::runtime_error("Invalid Airfoil Selection!.");
+  }
 
+  // Decompose it
+  int a = NACA / 10000;
+  int b = (NACA / 1000) % 10;
+  int c = (NACA / 100) % 10;
+  int d = (NACA / 10) % 10;
+  int e = NACA % 10;
 
-  if (a == 0 && b == 0 && c ==0) // 4-DIGIT SYMMETRIC AIRFOIL
-  {nacasym4(Points,dist,d,e);}
-//  else if  // 4- DIGIT NON SYMMETRIC AIRFOIL 
-//  {naca4(Points,dist,b,c,d,e)}
-//  else  // 5 - DIGIT AIRFOIL
-//  {naca5(Point,dist,a,b,c,d,e)}
- 
+  if (a == 0 && b == 0 && c == 0) // 4-DIGIT SYMMETRIC AIRFOIL
+  {
+    nacasym4(Points, num_panel, d, e);
+  }
+  else // 4- DIGIT NON SYMMETRIC AIRFOIL
+  {
+    nacaun4(Points, num_panel, b, c, d, e);
+  }
+  //  else  // 5 - DIGIT AIRFOIL
+  //  {naca5(Point,dist,a,b,c,d,e)}
 };
 
 
-double y_t_sym(double x,const double t) {
-  const double a0=0.2969, a1=-.126, a2=0.3516, a3=0.2843, a4=0.1036;
-  double y_t = 5.*t*(a0*std::sqrt(x)-a1*x-a2*x*x+a3*x*x*x-a4*x*x*x*x);
+
+void InitFromNaca::nacaun4(std::vector<Point> &Points, const int num_panel, const int b, const int c, const int d, const int e)
+{
+
+  double m = 0.01 * static_cast<double>(b);
+  double p = 0.1 * static_cast<double>(c);
+  double t = 0.1 * static_cast<double>(d) + 0.01 * static_cast<double>(e);
+
+  int lower_panel = num_panel / 2;
+  int upper_panel = num_panel - lower_panel;
+
+  Points.resize(num_panel + 1);
+
+  auto dist = arma::linspace(2. * acos(0.0), 0, lower_panel + 1);
+  dist = 0.5 * (1. - cos(dist));
+
+  int i = 0;
+  double xp, yp;
+  double yt, theta;
+
+  for (double x : dist)
+  {
+    yt = calc_y_t(x, t);
+    theta = calcTheta(m, p, x);
+    xp = x + yt * sin(theta);
+    yp = calc_y_c(m, p, x) - yt * cos(theta);
+    Points[i].setValues(xp, yp);
+    i++;
+  }
+
+  dist = arma::linspace(dist[lower_panel - 3], 2. * acos(0.0), upper_panel);
+  dist = 0.5 * (1. - cos(dist));
+
+  for (double x : dist)
+  {
+    yt = calc_y_t(x, t);
+    theta = calcTheta(m, p, x);
+    xp = x - yt * sin(theta);
+    yp = calc_y_c(m, p, x) + yt * cos(theta);
+    Points[i].setValues(xp, yp);
+    i++;
+  }
+}
+
+void InitFromNaca::nacasym4(std::vector<Point> &Points, const int num_panel, int d, int e)
+{
+  double t = 0.1 * static_cast<double>(d) + 0.01 * static_cast<double>(e);
+
+  int lower_panel = num_panel / 2;
+  int upper_panel = num_panel - lower_panel;
+
+  Points.resize(num_panel + 1);
+
+  auto dist = arma::linspace(2. * acos(0.0), 0, lower_panel + 1);
+  dist = 0.5 * (1. - cos(dist));
+
+  int i = 0;
+  for (double x : dist)
+  {
+    Points[i].setValues(x, -calc_y_t(x, t));
+    i++;
+  }
+
+  dist = arma::linspace(dist[lower_panel - 3], 2. * acos(0.0), upper_panel);
+  dist = 0.5 * (1. - cos(dist));
+
+  for (double x : dist)
+  {
+    Points[i].setValues(x, calc_y_t(x, t));
+    i++;
+  }
+}
+
+double calc_y_t(double x, const double t)
+{
+  const double a0 = 0.2969, a1 = -.126, a2 = -0.3516, a3 = 0.2843, a4 = -0.1036;
+  double y_t = 5. * t * (a0 * std::sqrt(x) + a1 * x + a2 * x * x + a3 * x * x * x + a4 * x * x * x * x);
   return y_t;
 }
 
-void InitFromNaca::nacasym4(std::vector<Point> &Points,const arma::vec &dist,int d,int e){
-  
-  double t = d*10 + e;
-  
-  int i=0;
-  for (auto x : dist)
+double calcTheta(double m, double p, double x)
+{
+  double theta;
+  if (x <= p)
   {
-    Points.emplace_back(x,y_t_sym(x,t));
+    theta = atan(2. * m / (p * p) * (p - x));
   }
-}
+  else
+  {
+    theta = atan(2. * m / ((1. - p) * (1. - p)) * (p - x));
+  }
+  return theta;
+};
+
+double calc_y_c(double m, double p, double x)
+{
+  double yc;
+  if (x <= p)
+  {
+    yc = m / (p * p) * (2. * p * x - x * x);
+  }
+  else
+  {
+    yc = m / ((1. - p) * (1. - p)) * ((1. - 2. * p) + 2. * p * x - x * x);
+  }
+  return yc;
+};
--- src/InitFromNaca.hpp
+++ src/InitFromNaca.hpp
@@ -9,7 +9,13 @@ class InitFromNaca : public BaseInitializer
 {
 public:
   void initPoints(std::vector<Point>&) override;
-  void nacasym4(std::vector<Point>&,const arma::vec&,int,int);
+  void nacasym4(std::vector<Point>&,const int,const int,const int);
+  void nacaun4(std::vector<Point> &,const int,const int,const int,const int,const int);
 };
+
+
+double calc_y_t(double,const double) ;
+double calcTheta(double,double,double);
+double calc_y_c(double,double,double);
 
 #endif
